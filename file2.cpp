387b0fc44aacc3f3725d39219968dffa4cad0c9f
NekoKaicho <adecta7@gmail.com>
2026-03-03 21:25:38 +0700

    int main dan selesai

--- file2.cpp
+++ file2.cpp
@@ -11,13 +11,24 @@ void input() {
     cout << "Masukkan lebar = ";
     cin >> l;
 }
+
 float jumlah(float a, float b, float c) {
     return a + b + c;
 }
+
 int luasPersegiP(int a, int b) {
     return a * b;
 }
+
 void output() {
     cout << "Hasilnya = " << luasPersegiP(p, l) << endl;
     cout << "terima kasih" << endl;
 }
+
+int main() { //mulai
+    input();
+    output();
+    cout << "hasil penjumlahan = " << jumlah(15.1, 24.9, 67) << endl;
+    
+    return 0;
+} //selesai
