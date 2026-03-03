d25503b24174d8687597a9f06663ea590d0537ee
NekoKaicho <adecta7@gmail.com>
2026-03-03 21:23:15 +0700

    Deklarasi global dan implementasi prosedur dan fungsi

---
+++ file2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// deklarasi global
+int p, l;
+
+// Implementasi Prosedur dan Fungsi
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
